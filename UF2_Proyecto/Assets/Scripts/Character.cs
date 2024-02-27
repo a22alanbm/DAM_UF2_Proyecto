f1570e3a68747aaf5f77583dd7ef4715991882ed
A22AlanBM <a22alanbm@a21eql06.sanclemente.local>
2024-02-27 13:30:47 +0100

    CambiosAnimacionesYMecanicas

--- UF2_Proyecto/Assets/Scripts/Character.cs
+++ UF2_Proyecto/Assets/Scripts/Character.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float jumpForce = 10f;
+    public float jumpForce = 20f;
     public float impulsoVoltereta = 5f;
     [SerializeField] LayerMask sueloMask;
-    
+
     public BoxCollider2D boxCollider2D;
 
     private Animator animator;
@@ -17,6 +17,7 @@ public class Character : MonoBehaviour
     private bool isFacingRight = true;
     private bool puedeRealizarAcciones = true;
     private bool haciendoVoltereta = false;
+    private bool defendiendo = false;
 
     void Start()
     {
@@ -28,7 +29,17 @@ public class Character : MonoBehaviour
 
     void Update()
     {
-        // Verificar si está en el suelo
+
+        if (transform.position.x >= 115.3f)
+    {
+        // Resta 245 de la posición x
+        transform.position = new Vector3(transform.position.x - 213f, transform.position.y, transform.position.z);
+    }
+    if (transform.position.x <= -97.7f)
+    {
+        // Resta 245 de la posición x
+        transform.position = new Vector3(transform.position.x + 213f, transform.position.y, transform.position.z);
+    }
 
         if (haciendoVoltereta)
         {
@@ -36,50 +47,62 @@ public class Character : MonoBehaviour
             float movimientoVoltereta = impulsoVoltereta * (isFacingRight ? 1f : -1f);
             rb.velocity = new Vector2(movimientoVoltereta, rb.velocity.y);
         }
+        else if (defendiendo){}
         else
         {
             float horizontalInput = Input.GetAxis("Horizontal");
 
             // Movimiento lateral y lógica de voltear solo si está en el suelo
-            
-                // Movimiento lateral normal
-                Vector2 movement = new Vector2(horizontalInput, 0f);
-                rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
 
-                // Lógica de voltear
-                if (horizontalInput > 0 && !isFacingRight)
-                {
-                    Flip();
-                    animator.SetBool("Andar", true);
-                }
-                else if (horizontalInput < 0 && isFacingRight)
-                {
-                    Flip();
-                    animator.SetBool("Andar", true);
-                }
-                else
-                {
-                    animator.SetBool("Andar", false);
-                }
-                // Salto con la tecla espacio solo si está en el suelo
-                if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
-                {
-                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                }
+            // Movimiento lateral normal
+            Vector2 movement = new Vector2(horizontalInput, 0f);
+            rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+
+            // Lógica de voltear
+            if (horizontalInput > 0 && !isFacingRight)
+            {
+                Flip();
+                animator.SetBool("Andar", true);
+            }
+            else if (horizontalInput < 0 && isFacingRight)
+            {
+                Flip();
+                animator.SetBool("Andar", true);
+            }
+            else
+            {
+                animator.SetBool("Andar", false);
+            }
+
+
+            if (IsGrounded() && Input.GetKeyDown(KeyCode.K) )
+            {
+                animator.SetTrigger("Daño");
+            }
+
+            // Salto con la tecla espacio solo si está en el suelo
+            if (IsGrounded() && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.UpArrow)))
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            }
 
             // Voltereta con la tecla R solo si está en el suelo
             if (IsGrounded() && Input.GetKeyDown(KeyCode.R))
             {
                 StartCoroutine(IniciarVoltereta());
             }
+            if (IsGrounded() && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
+            {
+                StartCoroutine(Defender());
+            }
 
             // Lógica de animación de salto y caída
-            if (rb.velocity.y > 0)
+            if (!IsGrounded() && rb.velocity.y > 0)
             {
                 animator.SetBool("Saltar", true);
                 animator.SetBool("Caer", false);
             }
-            else if (rb.velocity.y < 0)
+            else if (!IsGrounded() && rb.velocity.y < 0)
             {
                 animator.SetBool("Saltar", false);
                 animator.SetBool("Caer", true);
@@ -104,9 +127,9 @@ public class Character : MonoBehaviour
         rb.velocity = new Vector2(0f, rb.velocity.y);
 
         haciendoVoltereta = true;
-        animator.SetBool("Voltereta", true);
+        animator.SetTrigger("Voltereta");
 
-        yield return new WaitForSeconds(0.7f);
+        yield return new WaitForSeconds(0.5f);
 
         FinalizarVoltereta();
     }
@@ -114,13 +137,32 @@ public class Character : MonoBehaviour
     // Método llamado por la animación al finalizar la voltereta
     public void FinalizarVoltereta()
     {
-        animator.SetBool("Voltereta", false);
+        
         haciendoVoltereta = false;
     }
 
-    public bool IsGrounded(){
-        RaycastHit2D rayhit= Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0, Vector2.down, 0.1f, sueloMask);
-        return rayhit.collider!=null;
+    public bool IsGrounded()
+    {
+        RaycastHit2D rayhit = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0, Vector2.down, 0.1f, sueloMask);
+        return rayhit.collider != null;
+    }
+
+    public IEnumerator Defender()
+    {
+        animator.SetTrigger("Defensa");
+        defendiendo = true;
+        yield return new WaitForSeconds(2.5f);
+        defendiendo = false;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // Si la etiqueta de colisión del objeto que choca es "daño"...
+        if (collision.gameObject.tag == "daño")
+        {
+            // ...activar el trigger "SetTriggerDaño".
+            animator.SetTrigger("Daño");
+        }
     }
 
 }
---
+++ UF2_Proyecto/Assets/Scripts/SeguirPersonaje.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class SeguirPersonaje : MonoBehaviour
+{
+    public Transform objetivo;
+    public float suavizadoMinimo = 0.3f; // Suavizado mínimo
+    public float suavizadoMaximo = 1.0f; // Suavizado máximo
+    public float distanciaSinSuavizado = 5.0f; // Distancia a partir de la cual no hay suavizado
+
+    private void FixedUpdate()
+    {
+        if (objetivo != null)
+        {
+            // Calcular la distancia entre la cámara y el objetivo
+            float distancia = Vector2.Distance(transform.position, objetivo.position);
+
+            // Calcular la posición deseada de la cámara
+            Vector3 posicionDeseada = new Vector3(objetivo.position.x, objetivo.position.y, transform.position.z);
+
+            // Ajustar la suavidad basada en la distancia
+            float suavizado = Mathf.Lerp(suavizadoMinimo, suavizadoMaximo, Mathf.InverseLerp(0, distanciaSinSuavizado, distancia));
+
+            // Suavizar el movimiento de la cámara
+            transform.position = Vector3.Lerp(transform.position, posicionDeseada, suavizado);
+        }
+    }
+}
