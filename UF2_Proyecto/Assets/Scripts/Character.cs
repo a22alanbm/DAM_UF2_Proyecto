6f30f85a4d60946ecb3840722a450e0db268f84b
a22alanbm <a22alanbm@iessanclemente.net>
2024-02-28 22:11:48 +0100

    AtaqueAereoConseguido

--- UF2_Proyecto/Assets/Scripts/Character.cs
+++ UF2_Proyecto/Assets/Scripts/Character.cs
@@ -29,88 +29,98 @@ public class Character : MonoBehaviour
 
     void Update()
     {
+        if(puedeRealizarAcciones){
 
         if (transform.position.x >= 115.3f)
-    {
-        // Resta 245 de la posición x
-        transform.position = new Vector3(transform.position.x - 213f, transform.position.y, transform.position.z);
-    }
-    if (transform.position.x <= -97.7f)
-    {
-        // Resta 245 de la posición x
-        transform.position = new Vector3(transform.position.x + 213f, transform.position.y, transform.position.z);
-    }
-
-        if (haciendoVoltereta)
         {
-            // Si está haciendo la voltereta, aplicar un pequeño movimiento lateral
-            float movimientoVoltereta = impulsoVoltereta * (isFacingRight ? 1f : -1f);
-            rb.velocity = new Vector2(movimientoVoltereta, rb.velocity.y);
+            // Resta 245 de la posición x
+            transform.position = new Vector3(transform.position.x - 213f, transform.position.y, transform.position.z);
         }
-        else if (defendiendo){}
-        else
+        if (transform.position.x <= -97.7f)
         {
-            float horizontalInput = Input.GetAxis("Horizontal");
+            // Resta 245 de la posición x
+            transform.position = new Vector3(transform.position.x + 213f, transform.position.y, transform.position.z);
+        }
 
-            // Movimiento lateral y lógica de voltear solo si está en el suelo
-
-            // Movimiento lateral normal
-            Vector2 movement = new Vector2(horizontalInput, 0f);
-            rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
-
-            // Lógica de voltear
-            if (horizontalInput > 0 && !isFacingRight)
+            if (haciendoVoltereta)
             {
-                Flip();
-                animator.SetBool("Andar", true);
+                // Si está haciendo la voltereta, aplicar un pequeño movimiento lateral
+                float movimientoVoltereta = impulsoVoltereta * (isFacingRight ? 1f : -1f);
+                rb.velocity = new Vector2(movimientoVoltereta, rb.velocity.y);
             }
-            else if (horizontalInput < 0 && isFacingRight)
-            {
-                Flip();
-                animator.SetBool("Andar", true);
-            }
+            else if (defendiendo){}
             else
             {
-                animator.SetBool("Andar", false);
-            }
+                float horizontalInput = Input.GetAxis("Horizontal");
+
+                // Movimiento lateral y lógica de voltear solo si está en el suelo
+
+                // Movimiento lateral normal
+                Vector2 movement = new Vector2(horizontalInput, 0f);
+                rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
+
+                // Lógica de voltear
+                if (horizontalInput > 0 && !isFacingRight)
+                {
+                    Flip();
+                    animator.SetBool("Andar", true);
+                }
+                else if (horizontalInput < 0 && isFacingRight)
+                {
+                    Flip();
+                    animator.SetBool("Andar", true);
+                }
+                else
+                {
+                    animator.SetBool("Andar", false);
+                }
 
 
-            if (IsGrounded() && Input.GetKeyDown(KeyCode.K) )
-            {
-                animator.SetTrigger("Daño");
-            }
+                if (IsGrounded() && Input.GetKeyDown(KeyCode.K) )
+                {
+                    animator.SetTrigger("Daño");
+                }
 
-            // Salto con la tecla espacio solo si está en el suelo
-            if (IsGrounded() && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.UpArrow)))
-            {
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            }
+                // Salto con la tecla espacio solo si está en el suelo
+                if (IsGrounded() && (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)|| Input.GetKeyDown(KeyCode.UpArrow)))
+                {
+                    rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                }
 
-            // Voltereta con la tecla R solo si está en el suelo
-            if (IsGrounded() && Input.GetKeyDown(KeyCode.R))
-            {
-                StartCoroutine(IniciarVoltereta());
-            }
-            if (IsGrounded() && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
-            {
-                StartCoroutine(Defender());
-            }
+                // Voltereta con la tecla R solo si está en el suelo
+                if (IsGrounded() && Input.GetKeyDown(KeyCode.R))
+                {
+                    StartCoroutine(IniciarVoltereta());
+                }
+                if (IsGrounded() && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
+                {
+                    StartCoroutine(Defender());
+                }
 
-            // Lógica de animación de salto y caída
-            if (!IsGrounded() && rb.velocity.y > 0)
-            {
-                animator.SetBool("Saltar", true);
-                animator.SetBool("Caer", false);
-            }
-            else if (!IsGrounded() && rb.velocity.y < 0)
-            {
-                animator.SetBool("Saltar", false);
-                animator.SetBool("Caer", true);
-            }
-            else
-            {
-                animator.SetBool("Saltar", false);
-                animator.SetBool("Caer", false);
+            
+
+                if (Input.GetKeyDown(KeyCode.Q) && !IsGrounded())
+                {
+                    StartCoroutine( AtaqueAire() );
+                }else{
+
+                    // Lógica de animación de salto y caída
+                    if (!IsGrounded() && rb.velocity.y > 0)
+                    {
+                        animator.SetBool("Saltar", true);
+                        animator.SetBool("Caer", false);
+                    }
+                    else if (!IsGrounded() && rb.velocity.y < 0)
+                    {
+                        animator.SetBool("Saltar", false);
+                        animator.SetBool("Caer", true);
+                    }
+                    else
+                    {
+                        animator.SetBool("Saltar", false);
+                        animator.SetBool("Caer", false);
+                    }
+                }
             }
         }
     }
@@ -131,13 +141,6 @@ public class Character : MonoBehaviour
 
         yield return new WaitForSeconds(0.5f);
 
-        FinalizarVoltereta();
-    }
-
-    // Método llamado por la animación al finalizar la voltereta
-    public void FinalizarVoltereta()
-    {
-        
         haciendoVoltereta = false;
     }
 
@@ -164,5 +167,21 @@ public class Character : MonoBehaviour
             animator.SetTrigger("Daño");
         }
     }
+public IEnumerator AtaqueAire()
+    {
+        puedeRealizarAcciones = false;
+        animator.SetBool("Saltar", false);
+        animator.SetBool("Caer", false);
+        float gravity = rb.gravityScale;
+        if (rb.velocity.y > 0){
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
+        }
+            rb.gravityScale= 0.1f;
+        animator.SetTrigger("AirAtk");
+        yield return new WaitForSeconds(1f);
+        rb.gravityScale = gravity;
+        puedeRealizarAcciones = true;
+    }
+
 
 }
